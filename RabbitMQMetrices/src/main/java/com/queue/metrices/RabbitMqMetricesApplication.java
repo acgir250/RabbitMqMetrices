27b68dc3952cc1d3b20753e7723c17775b05b43a
Girish <Girish@Girish-PC>
2019-08-17 11:29:57 +0530

    first commit

---
+++ RabbitMQMetrices/src/main/java/com/queue/metrices/RabbitMqMetricesApplication.java
@@ -0,0 +1,13 @@
+package com.queue.metrices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqMetricesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqMetricesApplication.class, args);
+	}
+
+}
